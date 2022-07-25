d5c7c406d1e6441f508c75f6a6ce8656238c7c7d
fredr <fredr@DESKTOP-DP9PI9A>
2022-07-25 12:52:54 +0200

    Updated referral parser to generate sample data

--- src/Referral/LabAware.Referral.Parse/ReferralParser.cs
+++ src/Referral/LabAware.Referral.Parse/ReferralParser.cs
@@ -9,14 +9,35 @@ public class ReferralParser : IReferralParser
         format.Replace(".", "").ToUpperInvariant() switch
         {
             "JSON" => JsonContent(content),
-            _ => throw new InvalidOperationException($"Format '{format}' is not recognized as a valid referral format.")
+            _ => Sample(content)
+            //_ => throw new InvalidOperationException($"Format '{format}' is not recognized as a valid referral format.")
         };
 
     private static async Task<ErrorOr<Referral>> JsonContent(Stream content)
     {
         try
         {
-            throw new NotImplementedException();
+            return new Referral(
+                Guid.NewGuid().ToString(),
+                new PatientInfo(Guid.NewGuid().ToString(), "Abc Def", DateTime.Now.AddYears(-60)),
+                new Study("Study1"),
+                new Sample[] { new Sample(Guid.NewGuid().ToString(), "SampleType1") });
+        }
+        catch (Exception ex)
+        {
+            return Error.Failure(ex.Message);
+        }
+    }
+
+    private static async Task<ErrorOr<Referral>> Sample(Stream content)
+    {
+        try
+        {
+            return new Referral(
+                Guid.NewGuid().ToString(),
+                new PatientInfo(Guid.NewGuid().ToString(), "Abc Def", DateTime.Now.AddYears(-60)),
+                new Study("Study1"),
+                new Sample[] { new Sample(Guid.NewGuid().ToString(), "SampleType1") });
         }
         catch (Exception ex)
         {
