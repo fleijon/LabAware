85ab80fb1d49143a6cec017096533bea1e63d712
fredr <fredr@DESKTOP-DP9PI9A>
2022-07-25 12:54:31 +0200

    Removed dead refs

--- src/App/LabAware.Host/Program.cs
+++ src/App/LabAware.Host/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using LabAware.Host.Data;
 using LabAware.Referral.Features;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddReferralFeatures();
 
 var app = builder.Build();
